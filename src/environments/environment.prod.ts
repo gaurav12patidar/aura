ceb0de18bbb78d64d15933f0a28204e293d9000b
Gaurav Patidar <55226497+gaurav12patidar@users.noreply.github.com>
2021-04-13 11:05:26 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBVcHfrA2Z7NfhTawr3QDMYdWrlZypWe2A",
+    authDomain: "aura-eb3ef.firebaseapp.com",
+    projectId: "aura-eb3ef",
+    storageBucket: "aura-eb3ef.appspot.com",
+    messagingSenderId: "1050481178209",
+    appId: "1:1050481178209:web:292d61709e6ca72b05dc20",
+    measurementId: "G-J113WY3QZS"
+  }
 };
